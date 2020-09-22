7097af9f9cd459ec049177fc0a45171f0d1a7128
Ben de Garcia <degarcia.ben@gmail.com>
2020-09-21 22:25:02 -0500

    added first cards

--- src/App.js
+++ src/App.js
@@ -1,12 +1,88 @@
-import React, {useState, useEffect} from 'react';
+import React from 'react';
 import './App.css';
-import employees from "./utils/employees"
+// import employees from "./utils/employees"
 import Card from "./utils/Emp_Card"
+
+const employees = [
+  {
+    id: 1,
+    firstName: "Adam",
+    lastName: "Jackson",
+    phone: "847-555-6620",
+    email: "ajackson@realfakedoors.com",
+  },
+  {
+    id: 2,
+    firstName: "Beth",
+    lastName: "Iverson",
+    phone: "847-555-6621",
+    email: "biverson@realfakedoors.com",
+  },
+  {
+    id: 3,
+    firstName: "Charles",
+    lastName: "Hoffman",
+    phone: "847-555-6622",
+    email: "choffman@realfakedoors.com",
+  },
+  {
+    id: 4,
+    firstName: "Devona",
+    lastName: "Giffin",
+    phone: "847-555-6623",
+    email: "dgriffin@realfakedoors.com",
+  },
+  {
+    id: 5,
+    firstName: "Ericka",
+    lastName: "Fuller",
+    phone: "847-555-6624",
+    email: "efuller@realfakedoors.com",
+  },
+  {
+    id: 6,
+    firstName: "Frank",
+    lastName: "Elliot",
+    phone: "847-555-6625",
+    email: "felliot@realfakedoors.com",
+  },
+  {
+    id: 7,
+    firstName: "Guy",
+    lastName: "Dixon",
+    phone: "  847-555-6626",
+    email: "gdixon@realfakedoors.com",
+  },
+  {
+    id: 8,
+    firstName: "Harold",
+    lastName: "Carter",
+    phone: "847-555-6627",
+    email: "hcarter@realfakedoors.com",
+  },
+  {
+    id: 9,
+    firstName: "Isabelle",
+    lastName: "Baker",
+    phone: "847-555-6628",
+    email: "bisabelle@realfakedoors.com",
+  },
+  {
+    id: 10,
+    firstName: "Jill",
+    lastName: "Avila",
+    phone: "847-555-6629",
+    email: "jchan@realfakedoors.com",
+  }
+];
 
 function App() {
   return (
     <>
-    <Card />
+    <div className="jumbotron jumbotron-fluid text-center">
+      <h1>Real Fake Doors Employee Directory</h1>
+    </div>
+    <Card employees={employees} />
     </>
   );
 }
--- src/utils/Emp_Card.js
+++ src/utils/Emp_Card.js
@@ -1,10 +1,32 @@
-import React from 'react'
+import React from "react";
 
-function Card(props){
-    return (
-        <p>Test</p>
-    )
-
+function Card(props) {
+  console.log(props);
+  return (
+    <div>
+      {props.employees.map((person) => (
+        <div className="card text-white bg-info mb-3">
+          <div class="card-header">
+            {person.firstName} {person.lastName}
+          </div>
+          <div class="card-body">
+            <p>{person.email}</p>
+            <p>{person.phone}</p>
+            </div>
+        </div>
+      ))}
+    </div>
+  );
 }
 
-export default Card
+export default Card;
+
+// {/* <div class="card text-white bg-info mb-3" style="max-width: 18rem;">
+  
+//     <h5 class="card-title">Info card title</h5>
+//     <p class="card-text">
+//       Some quick example text to build on the card title and make up the bulk of
+//       the card's content.
+//     </p>
+//   </div>
+// </div>; */}
